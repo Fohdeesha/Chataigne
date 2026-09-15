55d071802d016ab9a304a1e0405bf3920bd02ceb
Jon Sands <fohdeesha@gmail.com>
2026-09-15 04:19:48 -0400

    Commit the generated VS2022 precompiled-header stubs and packages.config
    The .jucer lists a precompiled header and AppConfig.h forces WebView2 on, but neither
    the Projucer-generated PCH pair nor the NuGet packages.config was in the tree, so the
    VisualStudio2022 exporter did not build from a fresh checkout.

---
+++ Builds/VisualStudio2022/JucePrecompiledHeader_Debug.h
@@ -0,0 +1,25 @@
+/*
+
+    IMPORTANT! This file is auto-generated each time you save your
+    project - if you alter its contents, your changes may be overwritten!
+
+*/
+
+#ifndef JUCE_SKIP_PRECOMPILED_HEADER
+
+/*
+  ==============================================================================
+
+    PrecompiledHeaders.h
+    Created: 14 Aug 2025 5:59:07pm
+    Author:  yoanncatala
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+/* We can add here any headers that may be frequently used to get PCH benefits here too! */
+#endif
